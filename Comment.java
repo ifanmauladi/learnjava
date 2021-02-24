004f1789c1e870956aa11048d19f7c9524639020
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-02-25 05:44:44 +0700

    Create Comment.java file

---
+++ Comment.java
@@ -0,0 +1,8 @@
+public class Comment{
+  public station void main(String[] arcs){
+    //this is a comment
+    System.out.println("welcome");
+    /*comment is used to inactive the code, create 
+    code is readable */
+  }
+}
