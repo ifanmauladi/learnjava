ddc82d19ad385e7ef1e1c70b491fda3591e00cfd
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-02-25 11:36:07 +0700

    Create DataType.java file

---
+++ DataType.java
@@ -0,0 +1,27 @@
+public class DataType{
+  public static void main(String[] args){
+    System.out.println("we will learn about data types");
+    //kind of datatypes on java
+    //byte range is 1 byte
+    byte valueByte= 15;
+    System.out.println(valueByte);
+    //short range is 2 byte
+    short valueShort= 1500;
+    System.out.println(valueShort);
+    //char range is 2 byte
+    char valueChar= 100;
+    System.out.println(valueChar);
+    //int range is 4 byte
+    int valueInt= 15750;
+    System.out.println(valueInt);
+    //long range is 8 byte
+    long valueLong= 275000l;
+    System.out.println(value long);
+    //float range is 16 byte
+    float valueFloat= 20.56f;
+    System.out.println(valueFloat);
+    //double range is 32 byte
+    double valueDouble= 75.678d;
+    System.out.println(value double);
+  }
+}
