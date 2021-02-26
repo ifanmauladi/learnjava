5616d496bfb0979b03bcd4ec9c374c90b6a39a1e
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-02-26 11:16:50 +0700

    create operator.java file

---
+++ Operator.java
@@ -0,0 +1,20 @@
+public class Operator{
+  public static void main(String[] arcs){
+    System.out.println("we will learn the operator in java");
+    byte x = 12, y = 15;
+    //arithmetic operators
+    byte z = x + y;
+    z = x - y;
+    z = x * y;
+    z = x / y;
+    z = y % x;
+    z = y--;
+    z = x++;
+    //assignment operators
+    y += 5;
+    y -= 3;
+    y *= 3;
+    y /= 2;
+    //coming soon
+  }
+}
