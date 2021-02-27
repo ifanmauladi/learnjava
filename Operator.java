57a59d7685e4c0f29a11a86c2a7afee8618820d2
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-02-27 21:51:27 +0700

    add a comparison operator in file.

--- Operator.java
+++ Operator.java
@@ -1,5 +1,10 @@
 public class Operator{
   public static void main(String[] args){
+    System.out.println("welcome to comparison");
+    int x,y;
+    x = 22;
+    y = 25;
+    //gratherthan
     
   }
 }
