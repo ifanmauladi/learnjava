471f9b12938fc2048c12781aa45ac67380d7bb9e
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-02-28 07:40:32 +0700

    add comparison operators

--- Operator.java
+++ Operator.java
@@ -4,7 +4,16 @@ public class Operator{
     int x,y;
     x = 22;
     y = 25;
-    //gratherthan
-    
+    //gratherthan operator
+    //the comparism operator always value a boolean
+    boolean gratherthan = x > y; // values false
+    //lessthan operator
+    boolean lessthan = x < y; //values true
+    //gratherthan equals
+    boolean gEquals = x >= y; //values false
+    //lessthan equals
+    boolean lEquals = x <= y; //value true
+    //equals
+    boolean equals = x == y; // value false
   }
 }
