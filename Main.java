9cef1fa16824ec85dae0361280ddfdde3a952c66
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-03-01 08:59:34 +0700

    Create new Main.java file to remind me

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println("we will learn operator and datatype");
+    //Byte data type
+    byte valueByte = 100;
+    //arithmetic operator
+    System.out.println("1. arithmetic in byte");
+
+    System.out.println("- addition operator");
+    valueByte = (byte)(valueByte+2);
+    System.out.println(valueByte);
+
+    System.out.println("- substraction operator");
+    valueByte = (byte)(valueByte-7);
+    System.out.println(valueByte);
+  }
+}
