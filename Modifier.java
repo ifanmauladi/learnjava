61000039d22f1c234fdae601baf1a1a9a068dafb
Ifan Mauladi <79387572+ifanmauladi@users.noreply.github.com>
2021-03-05 10:47:55 +0700

    Create modifier file in java language

---
+++ Modifier.java
@@ -0,0 +1,11 @@
+public class Modifier{
+  public static void main(String[] args){
+    System.out.println("there are many modifier in java");
+    //public in attribute
+    public String name; 
+  }
+  //public in method
+  public static void take(){
+    System.out.println("test");
+  }
+}
